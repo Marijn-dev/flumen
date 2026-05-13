190f2fa8d4551bcd9d0f71a4e351b292acf15616
Marijn-dev <marijnruiter09@gmail.com>
2026-05-13 18:44:18 +0200

    update init

--- src/flumen/__init__.py
+++ src/flumen/__init__.py
@@ -1,4 +1,4 @@
 from .model import CausalFlowModel
-from .trajectory import TrajectoryDataset, RawTrajectoryDataset, ParamaterisedRawTrajectoryDataset, ParameterisedTrajectoryDataset
+from .trajectory import TrajectoryDataset, RawTrajectoryDataset
 from .train import validate
 from .utils import print_gpu_info
